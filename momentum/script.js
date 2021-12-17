6a771f7b126204c613576cbc5950940dd4f85104
potatochep <95287841+potatochep@users.noreply.github.com>
2021-12-17 22:26:32 +0900

    first study of javascript

---
+++ momentum/script.js
@@ -0,0 +1,38 @@
+const amIFat = null;
+let something;
+console.log(amIFat);
+console.log(something);
+
+const a = 5;
+const b = 20;
+let myName = "hyojin";
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log("My name is " + myName);
+
+myName = "Kim Hyo Jin ";
+
+console.log("my name is " + myName);
+
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const thu = "thu";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const daysOfWeek = [mon, tue, wed, thu, fri, sat, sun];
+
+console.log(daysOfWeek);
+
+// Get Item from Array
+console.log(daysOfWeek[4]);
+console.log(daysOfWeek[0]);
+console.log(daysOfWeek[6]);
+
+//add one item to the array
+daysOfWeek.push("holiday");
+console.log(daysOfWeek);
